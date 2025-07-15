bdc2c29811315c79d0125bbbc5586c3ed6974017
Francisco <frannsalinasdiaz@gmail.com>
2025-07-15 17:19:47 -0300

    Tipos de datos, operadores y condicionales en Js

--- hola-mundo.js
+++ hola-mundo.js
@@ -1,4 +1,11 @@
 console.log("hola mundo")
 
-const x = 2
-alert("hola")
+const x = 2 
+
+let Persona = {
+    nombre: "Ivan",
+    edad: 28,
+    actividades: ["Trabajar", "ver animes", "entrenar"]
+}
+
+console.log('la persona se llama: ' + Persona.nombre)
